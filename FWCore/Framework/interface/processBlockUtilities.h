0734c7c3d3e6410736d7deab938cb67c852ada3e
W. David Dagenhart <wdd@fnal.gov>
2021-03-27 00:08:15 +0100

    A little more

--- FWCore/Framework/interface/processBlockUtilities.h
+++ FWCore/Framework/interface/processBlockUtilities.h
@@ -8,15 +8,12 @@
 */
 
 #include <string>
-#include <vector>
 
 namespace edm {
 
   class Event;
 
   unsigned int eventProcessBlockIndex(Event const& event, std::string const& processName);
-  std::vector<unsigned int> const& eventProcessBlockIndexes(Event const& event);
-  std::vector<std::string> const& eventProcessesWithProcessBlockProducts(Event const& event);
 
 }  // namespace edm
 #endif
--- FWCore/Framework/src/processBlockUtilities.cc
+++ FWCore/Framework/src/processBlockUtilities.cc
@@ -8,10 +8,4 @@ namespace edm {
     return event.processBlockIndex(processName);
   }
 
-  std::vector<unsigned int> const& eventProcessBlockIndexes(Event const& event) { return event.processBlockIndexes(); }
-
-  std::vector<std::string> const& eventProcessesWithProcessBlockProducts(Event const& event) {
-    return event.processesWithProcessBlockProducts();
-  }
-
 }  // namespace edm
